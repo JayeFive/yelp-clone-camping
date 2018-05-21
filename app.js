e2b783c914f729237f9d4c2611e2196eff57f003
Johnny Olson <john.olson3@gmail.com>
2018-05-21 04:29:13 +0000

    add show route

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -18,7 +19,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //   {
 //     name: "Granite Hill", 
-//     image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1c80f31bb4040015d51db663252fbd30&auto=format&fit=crop&w=500&q=60"
+//     image: "https://images.unsplash.com/photo-1445308394109-4ec2920981b1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1c80f31bb4040015d51db663252fbd30&auto=format&fit=crop&w=500&q=60",
+//     description: "this place kicks ass"
 //   }, function(err, campground){
 //     if(err) console.log(err);
 //     else console.log("Newly created campground: " + campground);
@@ -34,7 +36,7 @@ app.get("/campgrounds", function(req, res) {
   
   Campground.find({}, function(err, allCampgrounds){
     if(err) console.log(err);
-    else res.render("campgrounds", {campgrounds: allCampgrounds});
+    else res.render("index", {campgrounds: allCampgrounds});
   })
 });
     
@@ -43,7 +45,8 @@ app.post("/campgrounds", function(req, res) {
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
 
   Campground.create(newCampground, function(err, newlyCreated) {
     if(err) {
@@ -56,6 +59,13 @@ app.post("/campgrounds", function(req, res) {
 
 app.get("/campgrounds/new", function(req, res) {
   res.render("new")
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if(err) console.log(err);
+    else res.render("show", {campground: foundCampground});
+  });
 });
 
     
