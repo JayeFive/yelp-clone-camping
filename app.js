6d08b41d7e9343002bf322d155f45a936320adfa
Johnny Olson <john.olson3@gmail.com>
2018-05-25 17:21:07 +0000

    add connect-flash

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express          = require("express"),
     app              = express(),
+    flash            = require("connect-flash"),
     bodyParser       = require("body-parser"),
     mongoose         = require("mongoose"),
     passport         = require("passport"),
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 // seedDB();
    
 //Passport config
