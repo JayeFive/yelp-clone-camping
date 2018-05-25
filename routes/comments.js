2644edf17595865b394501b21cd4401f956e446a
Johnny Olson <john.olson3@gmail.com>
2018-05-25 15:57:54 +0000

    finish crud routes for comments and add authorization

--- routes/comments.js
+++ routes/comments.js
@@ -34,12 +34,59 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
+// Edit
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
+    if(err) {
+      console.log(err);
+      res.redirect("back");
+    } else res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+  });
+});
+
+// Update
+router.put("/:comment_id/", checkCommentOwnership, function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    if(err){
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
+// Destroy
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err) console.log(err);
+    else res.redirect("/campgrounds/" + req.params.id);
+  });
+});
+
 //Middleware
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next){
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err) res.redirect("/campgrounds");
+      else {
+        if(foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
